500169ee886d72d5d57aa8bfd3746e64cc28cdc3
Kentaro Seki <kentaro.kpc@gmail.com>
2022-02-25 01:09:49 +0900

    後退代入

--- triangle_inverse/main.py
+++ triangle_inverse/main.py
@@ -12,14 +12,13 @@ def TRinv(A):
             B[i][j] = -B[i][i] * (a@b)
     return B
 
-
-if __name__ == '__main__':
-    np.random.seed(2)
-    n = 200
-    A = 3 * np.random.random((n,n))
-    for i in range(1,n):
-        A[i,:i] = np.zeros(i)
+def TRsolve(A, b):
+    x = np.zeros(b.shape)
+    for i in range(n-1,-1,-1):
+        x[i] = (b[i] - A[i]@x) / A[i][i]
+    return x
     
+def run_inv(A):
     def run(func):
         t0 = time.time()
         B = func(A)
@@ -33,4 +32,30 @@ if __name__ == '__main__':
     print("TRinv: ", t)
     print(LA.norm(correct-ans) / LA.norm(correct))
 
+def run_solve(A, b):
+    def run(func):
+        t0 = time.time()
+        B = func(A, b)
+        tf = time.time()
+        return (tf-t0),B
+
+    T,correct = run(LA.solve)
+    t,ans = run(TRsolve)
+
+    print("LA.inv:", T)
+    print("TRinv: ", t)
+    print(LA.norm(correct-ans) / LA.norm(correct))
+
+
+
+if __name__ == '__main__':
+    np.random.seed(2)
+    n = 200
+    A = 3 * np.random.random((n,n))
+    for i in range(1,n):
+        A[i,:i] = np.zeros(i)
+    b = np.random.random(n)
+
+    #run_inv(A)
+    run_solve(A, b)
 
